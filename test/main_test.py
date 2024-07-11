359f98f2a4d108073666afb64d9fd1d541436fe3
tungtv <vantung08@gmail.com>
2024-07-11 17:19:02 +0700

    minor update

--- test/main_test.py
+++ test/main_test.py
@@ -3,9 +3,8 @@ from main.main import main
 class TestClass:
     def test_main(self):
         assert main() == "Hello World"
-    
-    def test_char_main(self):
-        assert "H" in main()
+
+
 
 
 
