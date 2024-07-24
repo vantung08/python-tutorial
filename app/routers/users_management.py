fa2e742cb47b39f529e8ff6262ccb8507666ff2a
tungtv <vantung08@gmail.com>
2024-07-24 18:16:20 +0700

    adjust compose watch sync+restart

--- app/routers/users_management.py
+++ app/routers/users_management.py
@@ -3,24 +3,33 @@ from pymongo import MongoClient
 
 router = APIRouter()
 
-uri = "mongodb://mongodb:27017"
-client = MongoClient(uri)
-
 @router.post("/users/{user_name}")
-async def add_user(user_name, db: str = "mydb", cl: str = "user", age: int = 0, email: str = "TBD"):
-    database = client[db]
-    collection = database[cl]
-    document = {
-        "name": user_name,
-        "age": age,
-        "email": email
-    }
-    result = collection.insert_one(document)
-    return print(f"Document inserted with ID: {result.inserted_id}")
+async def add_user(user_name, database_name: str = "python-tutorial-mongodb", collection_name: str = "users", age: int = 0, gender: str = "TBD"):
+    uri = "mongodb://mongodb:27017"
+    client = MongoClient(uri)
+    try:
+        database = client[database_name]
+        collection = database[collection_name]
+        document = {
+            "name": user_name,
+            "age": age,
+            "gender": gender
+        }
+        result = collection.insert_one(document)
+        client.close()
+        return print(f"Document inserted with ID: {result.inserted_id}")
+    except Exception as e:
+        raise Exception("Unable to find the document due to the following error: ", e)
 
 @router.get("/users/{user_name}")
-async def get_user(user_name, db: str = "mydb", cl: str = "user"):
-    database = client.get_database(db)
-    collection = database.get_collection(cl)
-    user = collection.find_one({"name": user_name})
-    return print(user)
+async def get_user(user_name, database_name: str = "python-tutorial-mongodb", collection_name: str = "users"):
+    uri = "mongodb://mongodb:27017"
+    client = MongoClient(uri)
+    try:
+        database = client.get_database(database_name)
+        collection = database.get_collection(collection_name)
+        user = collection.find_one({"name": user_name})
+        client.close()
+        return print(user)
+    except Exception as e:
+        raise Exception("Unable to find the document due to the following error: ", e)
