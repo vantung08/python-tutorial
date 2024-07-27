4144fa318a2acafa630e19a8e54f698021996649
tungtv <vantung08@gmail.com>
2024-07-27 17:35:46 +0700

    add user apply pydantic model

--- app/routers/users_management.py
+++ app/routers/users_management.py
@@ -1,20 +1,22 @@
 from fastapi import APIRouter, HTTPException, Depends
 from motor.motor_asyncio import AsyncIOMotorClient
 from ..dependencies import get_mongo_client
+from pydantic import BaseModel
+
+class User(BaseModel):
+    name: str
+    age: int = 0
+    gender: str = "TBD"
+    email: str | None = None
 
 router = APIRouter()
 
-@router.post("/users/{user_name}")
-async def add_user(user_name, db_name: str = "python-tutorial-mongodb", collection_name: str = "users",
-                    age: int = 0, gender: str = "TBD", mongo_client: AsyncIOMotorClient = Depends(get_mongo_client)):
+@router.post("/users/")
+async def add_user(user: User, db_name: str = "python-tutorial-mongodb", collection_name: str = "users", mongo_client: AsyncIOMotorClient = Depends(get_mongo_client)):
     try:
         db = mongo_client[db_name]
         collection = db[collection_name]
-        document = {
-            "name": user_name,
-            "age": age,
-            "gender": gender
-        }
+        document = user.model_dump()
         result = await collection.insert_one(document)
         return print(f"Document inserted with ID: {result.inserted_id}")
     except Exception as e:
