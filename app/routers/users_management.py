f75ab343e8efb72bb30ca2f4edf7bf22b5896832
tungtv <vantung08@gmail.com>
2024-07-27 11:06:02 +0700

    change pymongo to motor for async application

--- app/routers/users_management.py
+++ app/routers/users_management.py
@@ -1,36 +1,33 @@
 from fastapi import APIRouter
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from ..core.config import settings
 
 router = APIRouter()
 
+client = AsyncIOMotorClient(settings.mongodb_uri)
+
 @router.post("/users/{user_name}")
-async def add_user(user_name, database_name: str = "python-tutorial-mongodb", collection_name: str = "users", age: int = 0, gender: str = "TBD"):
-    client = MongoClient(settings.mongodb_uri)
+async def add_user(user_name, db_name: str = "python-tutorial-mongodb", collection_name: str = "users",
+                    age: int = 0, gender: str = "TBD"):
     try:
-        database = client[database_name]
-        collection = database[collection_name]
+        db = client[db_name]
+        collection = db[collection_name]
         document = {
             "name": user_name,
             "age": age,
             "gender": gender
         }
-        result = collection.insert_one(document)
-        client.close()
+        result = await collection.insert_one(document)
         return print(f"Document inserted with ID: {result.inserted_id}")
     except Exception as e:
-        client.close()
         raise Exception("Unable to find the document due to the following error: ", e)
 
 @router.get("/users/{user_name}")
-async def get_user(user_name, database_name: str = "python-tutorial-mongodb", collection_name: str = "users"):
-    client = MongoClient(settings.mongodb_uri)
+async def get_user(user_name, db_name: str = "python-tutorial-mongodb", collection_name: str = "users"):
     try:
-        database = client.get_database(database_name)
-        collection = database.get_collection(collection_name)
-        user = collection.find_one({"name": user_name})
-        client.close()
+        db = client[db_name]
+        collection = db[collection_name]
+        user = await collection.find_one({"name": user_name})
         return print(user)
     except Exception as e:
-        client.close()
         raise Exception("Unable to find the document due to the following error: ", e)
