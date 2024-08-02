dfbb2201a1c4b0a33eefb3d03523b73749ce8a6e
tungtv <vantung08@gmail.com>
2024-08-02 15:46:13 +0700

    add response status code

--- app/api/routers/users.py
+++ app/api/routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from app.schema import UserIn, UserOut
 from app.models import User
 from app.crud import create_user, find_user
@@ -7,7 +7,7 @@ router = APIRouter(
     prefix="/users"
     )
 
-@router.post("/", response_model=UserOut)
+@router.post("/", response_model=UserOut, status_code=status.HTTP_201_CREATED)
 async def add_user(user: UserIn) -> User:
     try:
         created_user = await create_user(user)
@@ -16,7 +16,7 @@ async def add_user(user: UserIn) -> User:
     except Exception as e: # Required to refactor
         raise Exception(f"Unable to find the document due to the following error: {e}")
 
-@router.get("/{user_name}", response_model=list[UserOut])
+@router.get("/{user_name}", response_model=list[UserOut], status_code=status.HTTP_200_OK)
 async def get_user(user_name: str) -> list[User]:
     try:
         return await find_user(user_name)
