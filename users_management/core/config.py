6b43fe7ba0ecdce3f246102aa50643017c642dfb
tungtv <vantung08@gmail.com>
2025-01-24 14:47:30 +0700

    remove mongo in config.py

--- users_management/core/config.py
+++ users_management/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    mongodb_uri: str
+    # mongodb_uri: str
     # SECRET_KEY: str = secrets.token_urlsafe(32)
     SECRET_KEY: str = "XN4rx935RJ8EBsf5HGMprmAlDcLnbRp0Gd021dlA0fc"
     # 60 minutes * 24 hours * 8 days
